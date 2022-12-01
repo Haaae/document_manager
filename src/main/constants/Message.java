6c5b949c6001dd1edfb606c7c2223e54ec6bfcbe
Haaae <101785628+Haaae@users.noreply.github.com>
2022-12-02 05:00:48 +0900

    feat : 메세지를 관리하는 상수 클래스 구현

---
+++ src/main/constants/Message.java
@@ -0,0 +1,9 @@
+package main.constants;
+
+public final class Message {
+
+    public static final class Exception {
+        public static final String NOT_FOUND_DELETE_REPOSITORY = "해당 이름의 문서 저장소는 존재하지 않습니다.";
+    }
+
+}
